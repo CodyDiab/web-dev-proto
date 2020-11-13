dbeb542435cd8724b12eb37cfc6dd2e1ad5823a6
Diab <emailcodydiab@gmail.com>
2020-11-13 08:45:56 -0600

    jumbotron

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import './styles/index.css';
-import Header from './Components/Header'
+import Header from './Components/Header';
+import Jumbotron from "./Components/Jumbotron";
 
 function App() {
   return (
+    <>
  <Header/>
+ <Jumbotron/>
+   </>
   );
 }
 
---
+++ src/Components/Jumbotron.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Jumbotron(){
+
+
+    return(
+        <div className="jumbotron">
+            <h2 className="sub-heading">this will be some clever sub heading</h2>
+        </div>
+    )
+}
+
+export default Jumbotron;
